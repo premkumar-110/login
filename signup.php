0354a34ae5292d937d7915f96c935c21bb7de618
premkumar-110 <websitehostformysite@gmail.com>
2023-08-22 20:01:32 +0530

    Published

--- signup.php
+++ signup.php
@@ -9,7 +9,7 @@
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
     />
-    <link rel="stylesheet" href="signup.css" />
+    <link rel="stylesheet" href="signup.scss" />
     <link
       rel="stylesheet"
       href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"
